e8e492eab9072a9dd3b57f72a87dde0360f9867f
RAHUL-stack-arch <rk6007448@gmail.com>
2024-07-25 19:15:25 +0000

    learned about data_types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // it means treat all js code as newer version
+
+//alert(3 + 3) //can't run in compilor (nodejs) only for browser
+
+let name ="rahul" //String type
+let age = 20 //number type
+let isLoggedIn = false //Boolean type
+
+//Ranges 
+//number => 2 to power of 53 
+//for more big number we use  ' bigint ' 
+
+//undefined => means you have not defined value yet Ex-> let state;
+//null => it is a type for Ex-> let tempearture = null; this is used because 0 is also a temperature
+//so if there is error while collecting a temperatur details then show null ...
+//Therefor null is a 'Stand alone value'
+//symbol => use for uniqness 
+
+console.log(typeof "hello"); // gives a string
+//interview question
+console.log(typeof null); //gives a object
+console.log(typeof undefined) // gives a undefined
