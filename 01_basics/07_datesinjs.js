a960f8eb6dcb8ec3a7474d5bf7bf15c6bdb3c53c
RAHUL-stack-arch <rk6007448@gmail.com>
2024-08-10 19:20:45 +0000

    learn about dates

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,22 @@
+//dates
+//declaration of data
+let myDate = new Date()
+console.log(myDate);
+//typesof(myDate) = object
+
+
+//
+console.log(myDate.toString())
+
+//see by printing myDate.to-----()  it will give or remove a particular
+//exmple 
+// toDateString() = output -> March Jan 23 2023
+// toLocalString() = very important 
+
+//declare date object by instances
+let myDate2 = new Date(2023,11,8)
+console.log(myDate2.toLocaleString());
+
+//way 2
+let myDate3 = new Date(11-08-2024)
+console.log(myDate2)
